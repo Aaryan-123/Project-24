70ae8d6743e9e71a0b6066095658f4ef77d458dd
Aaryan <70795502+Aaryan-123@users.noreply.github.com>
2020-09-24 19:27:11 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,9 +1,18 @@
 class Paper {
-    constructor(){
-        this.body = Bodies.circle(50);
-        World.add(world,this.body);
-    }
-    display(){
-        ellipse(ball.position.x,ball.position.y,20,20);
+    constructor() {
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+        this.body = Bodies.circle(100, 648, 30, options);
+        this.radius = 30;
+        World.add(world, this.body);
+    } display() {
+        var pos = this.body.position;
+        fill("pink");
+        ellipseMode(CENTER);
+        ellipse(pos.x, pos.y, this.radius, this.radius);
     }
 }
