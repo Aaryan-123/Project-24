e6d1c77ab0eb733f52917ba6658b70cd79fc7a09
Aaryan <70795502+Aaryan-123@users.noreply.github.com>
2020-09-26 11:14:32 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -6,8 +6,8 @@ class Paper {
             friction: 0.5,
             density: 1.2
         }
-        this.body = Bodies.circle(100, 648, 30, options);
-        this.radius = 30;
+        this.body=Bodies.circle(100,648.8,30,options);
+        this.radius = 20;
         World.add(world, this.body);
     } display() {
         var pos = this.body.position;
