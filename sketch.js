c9078941a9075ef5bae52d5fc19fb5ff5e5e872e
Aaryan <70795502+Aaryan-123@users.noreply.github.com>
2020-09-27 11:09:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,49 +4,52 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload() {
+var ground;
+// var dustbin;
+function preload()
+{
 	
 }
 
 function setup() {
 	createCanvas(800, 700);
 
-
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
+    paper = new Paper();
+	ground = new Ground(width / 2, height - 50, width, 10);
 
-	ground = new Ground(width / 2,height - 50,width,10);
-
-	paper = new Paper();
-	dustbin1 = new Dustbin(550, height - 100, 10, 100);
-	dustbin2 = new Dustbin(750, height - 100, 10, 100);
-
-	dustbin3 = new Dustbin(650, height - 50, 200, 10);
+	dustbin = new Dustbin(500,600,10,100);
+	dustbin2 = new Dustbin(700,600,10,100);
+	dustbin3 = new Dustbin(600,650,200,10);
+	// var options = {
+	// 	isStatic : true,
+	// 	restitution : 0.3
+	// }
+	// dusbinObject = Bodies.rectangle(600,650,10,100,options);
+    // World.add(world,dusbinObject);
 
 	Engine.run(engine);
   
 }
 
-
 function draw() {
   rectMode(CENTER);
   background(0);
-  
   ground.display();
-
-  dustbin1.display();
+  paper.display();
+  dustbin.display();
   dustbin2.display();
   dustbin3.display();
-  paper.display();
+  
   drawSprites();
- 
 }
 
 function keyPressed() {
-	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x : 85,y : -85});
+	if(keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper.body,paper.body.position,{x : 145,y : -115});
 	}
 }
 
